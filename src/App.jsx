2284f780819404d2146ace985af23d68b8ec8b46
binmex <robin019@hotmail.com>
2024-01-27 16:09:49 -0500

    fixed bug footer relational on position screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import {NavPrime} from "./Components/NavPrime"
+import { NavPrime } from "./Components/NavPrime";
 import { BannerPrime } from "./Components/BannerPrime";
 import { FooterPrime } from "./Components/FooterPrime";
 
 function App() {
   return (
-    <div className="div-con-fondo">
-      <NavPrime/>
-      <BannerPrime/>
-      <FooterPrime/>
-    </div>
-    
+    <>
+      <div className="div-con-fondo">
+        <NavPrime />
+        <BannerPrime />
+      </div>
+      <FooterPrime />
+    </>
   );
 }
 
