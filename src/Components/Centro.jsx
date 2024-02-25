ab4c93677046a604d7244ee3d7d351cf1642986b
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-02-25 13:48:54 -0500

    update img preview

--- src/Components/Centro.jsx
+++ src/Components/Centro.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Steps } from 'primereact/steps';
 import { Button } from 'primereact/button'
 import { Editor } from "primereact/editor";
 
 import { ColorPicker } from 'primereact/colorpicker';
+import { API_URL, hexToRgb } from '../utils/constants';
 
 
 export const Centro = () => {
     const [text, setText] = useState('');
     const [activeIndex, setActiveIndex] = useState(0);
     const [color, setColor] = useState(null);
+    const [loading, setLoading] = useState(false)
+    const [promptvalidate, setPromptvalidate] = useState(false)
+    // const [colorsPickedColormind, setColorsPickedColormind] = useState({
+    //     color1: { r: 0, g: 0, b: 0 },
+    //     color2: { r: 0, g: 0, b: 0 },
+    //     color3: { r: 0, g: 0, b: 0 },
+    //     color4: { r: 0, g: 0, b: 0 },
+    //     color5: { r: 0, g: 0, b: 0 }
+    // })
+    const [roomDesign, setRoomDesign] = useState("");
+    const [dataColors, setDataColors] = useState({})
+
     const items = [
         {
             label: 'Ingresa Descripcion de tu habitacion'
@@ -24,6 +37,77 @@ export const Centro = () => {
             label: 'Disfruta de tu habitacion'
         }
     ];
+
+
+    const handleClick = async (e) => {
+        setLoading(true)
+        console.log(`the color is: ${color}`)
+
+        try {
+            const response = await fetch(`${API_URL}/nlp/validate`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ text: text }),
+            });
+            const data = await response.json();
+
+            data.sentiment.vote === "positive" ? setPromptvalidate(true) : alert(`prompt no valido: ${data.sentiment.vote} prompt: ${text}`)
+
+
+
+        }
+        catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    const convertColorToRGBArray = (color) => {
+
+        const colorrgb = hexToRgb(color)
+        console.log(`color rgb = ${colorrgb}`)
+        setColor(colorrgb)
+        return colorrgb
+    }
+    const handleSubmit = async (e) => {
+        console.log(`color update = ${color}`)
+
+        const response = await fetch(`${API_URL}/colormind`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ color: color })
+        })
+        console.log(response)
+        const data = await response.json()
+        console.log(data.data)
+        setDataColors(data.data)
+        console.log(`data colors: ${dataColors.color1}`)
+
+        try{
+            const response = await fetch(`${API_URL}/prodia`, {
+                method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ prompUser:`${text} whit colors: ${dataColors.color1}, ${dataColors.color2}, ${dataColors.color3}, ${dataColors.color4}, ${dataColors.color5}  ` }),
+            });
+            const data = await response.json();
+            console.log(data);
+            setRoomDesign(data.data.imageUrl);
+        }
+        catch (error) {
+            console.log(error);
+        }finally{
+            
+        }
+        
+
+    }
+
     return (
         <div className="contentLau">
 
@@ -38,7 +122,7 @@ export const Centro = () => {
                 <Button outlined={activeIndex !== 1} rounded label="2" onClick={() => setActiveIndex(1)} />
                 <Button outlined={activeIndex !== 2} rounded label="3" onClick={() => setActiveIndex(2)} />
                 <Button outlined={activeIndex !== 3} rounded label="4" onClick={() => setActiveIndex(3)} />
-                
+
 
                 <Steps className='steps' model={items} activeIndex={activeIndex} />
 
@@ -47,28 +131,33 @@ export const Centro = () => {
 
 
             <div className="editor-container">
-                <Editor value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '180px' }} />
+                <Editor value={text} onTextChange={(e) => setText(e.textValue)} style={{ height: '180px' }} />
             </div>
 
             <div>
                 <div className='divColor'>
                     <div>
-                    <ColorPicker value={color} onChange={(e) => setColor(e.value)} inline />
+                        <ColorPicker value={color} onChange={(e) => setColor(convertColorToRGBArray(e.value))} inline />
+
                     </div>
                     <div className='colorpicked'>
-                        <ColorPicker disabled />
-                        <ColorPicker disabled />
-                        <ColorPicker disabled />
-                        <ColorPicker disabled />
-                        <ColorPicker disabled />
+                        <div className='colorBox' style={{ backgroundColor: `${dataColors.color1}` }}></div>
+                        <div className='colorBox' style={{ backgroundColor: `${dataColors.color2}` }}></div>
+                        <div className='colorBox' style={{ backgroundColor: `${dataColors.color3}` }}></div>
+                        <div className='colorBox' style={{ backgroundColor: `${dataColors.color4}` }}></div>
+                        <div className='colorBox' style={{ backgroundColor: `${dataColors.color5}` }}></div>
+                        
                     </div>
                 </div>
 
 
                 <div className='buttonContent'>
-                    <Button label="Validar " />
+                    <Button disabled={loading} onClick={handleClick} label="Validar " />
 
-                    <Button label="Aceptar" severity="success" disabled />
+                    <Button label="Aceptar" severity="success" disabled={!promptvalidate} onClick={handleSubmit} />
+                </div>
+                <div className='room_img'>
+                {roomDesign !== "" && <img src={roomDesign} alt="room design" />}
                 </div>
 
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,23 @@
 export const API_URL = 'http://localhost:4000';
+
+export function hexToRgb(hex) {
+        
+    if (hex.length === 3) {
+        hex = hex
+            .split('')
+            .map(function (char) {
+                return char + char;
+            })
+            .join('');
+    }
+
+    
+    var bigint = parseInt(hex, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+
+    
+    return [r,g,b] 
+}
+
